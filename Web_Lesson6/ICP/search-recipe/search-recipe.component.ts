9d273d726dcb43bdf42a30aa59df8e168cfe61ac
Jakkula134 <60532837+Jakkula134@users.noreply.github.com>
2021-02-24 18:34:22 -0600

    ICP 6

---
+++ Web_Lesson6/ICP/search-recipe/search-recipe.component.ts
@@ -0,0 +1,72 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-search-recipe',
+  templateUrl: './search-recipe.component.html',
+  styleUrls: ['./search-recipe.component.css']
+})
+export class SearchRecipeComponent implements OnInit {
+  @ViewChild('recipe') recipes: ElementRef;
+  @ViewChild('place') places: ElementRef;
+  recipeValue: any;
+  placeValue: any;
+  venueList = [];
+  recipeList = [];
+  formattedAddress = [];
+
+  currentLat: any;
+  currentLong: any;
+  geolocationPosition: any;
+
+  recepieApi = 'https://api.edamam.com/search?q=';
+  recepieAppid = '&app_id=954fb537';
+  recepieKey = '&app_key=\n' +
+    '126ce360bf3f01d3b1698ac145e61a25\t';
+  placesApi = 'https://api.foursquare.com/v2/venues/search?';
+  clientdId = 'client_id=HHBRKDKKGRMEX10ZF4YOYO3GAUEDJGE3I55XUY1HG213R5UB';
+  clientSecret = '&client_secret=UFL2RBKMIJH1CYL400B5DDISODS2442Z1MT342HSDQACIWH0';
+  version = '&v=20180323';
+  near = '&near=';
+  query = '&query=';
+
+  constructor(private _http: HttpClient) {
+  }
+
+  ngOnInit() {
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.geolocationPosition = position;
+        this.currentLat = position.coords.latitude;
+        this.currentLong = position.coords.longitude;
+      });
+  }
+
+  getVenues() {
+
+    this.recipeValue = this.recipes.nativeElement.value;
+    this.placeValue = this.places.nativeElement.value;
+
+    if (this.recipeValue !== null) {
+      this._http.get( this.recepieApi + this.recipeValue + this.recepieAppid + this.recepieKey).subscribe((res: any) => {
+        console.log(res.hits);
+        this.recipeList = Object.keys(res.hits).map(function (k) {
+          const i = res.hits[k].recipe;
+          return {name: i.label, icon: i.image, url: i.url};
+        });
+      });
+    }
+
+    if (this.placeValue != null && this.placeValue !== '' && this.recipeValue != null && this.recipeValue !== '') {
+      this._http.get( this.placesApi  + this.clientdId + this.clientSecret + this.version
+        + this.near + this.placeValue).subscribe((res: any) => {
+        console.log(res);
+        this.venueList = Object.keys(res.response.venues).map(function (k) {
+          const i = res.response.venues[k];
+          return {name: i.name, formattedAddress: i.location.formattedAddress};
+        });
+      });
+    }
+  }
+}
